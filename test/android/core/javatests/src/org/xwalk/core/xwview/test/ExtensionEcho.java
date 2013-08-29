3dd9100a87aac6f320d6dd6c4aefebb5ea1d63d6
Halton Huo <halton.huo@intel.com>
2013-08-29 11:36:57 +0800

    [Android] Add ExtensionEchoTest for message passing unit test

---
+++ test/android/core/javatests/src/org/xwalk/core/xwview/test/ExtensionEcho.java
@@ -0,0 +1,39 @@
+// Copyright (c) 2013 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.xwalk.core.xwview.test;
+
+import org.xwalk.core.extensions.XWalkExtensionBridge;
+
+public class ExtensionEcho extends XWalkExtensionBridge {
+
+    public ExtensionEcho() {
+        super("echo",
+              "var echoListener = null;"
+              + "extension.setMessageListener(function(msg) {"
+              + "  if (echoListener instanceof Function) {"
+              + "    echoListener(msg);"
+              + "  };"
+              + "});"
+              + "exports.echo = function(msg, callback) {"
+              + "  echoListener = callback;"
+              + "  extension.postMessage(msg);"
+              + "};"
+              + "exports.syncEcho = function(msg) {"
+              + "  return extension.internal.sendSyncMessage(msg);"
+              + "};"
+             );
+    }
+
+    public void handleMessage(String message) {
+        postMessage("From java:" + message);
+    }
+
+    public String handleSyncMessage(String message) {
+        return "From java:" + message;
+    }
+
+    public void onDestroy() {
+    }
+}
