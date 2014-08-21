89a0d71b0094fe0c8f6bd63c092ae384504f4998
Halton Huo <halton.huo@intel.com>
2014-08-21 10:48:43 +0800

    Hack to enable remote debugging for runtime

--- runtime/android/core_internal/src/org/xwalk/core/internal/XWalkContent.java
+++ runtime/android/core_internal/src/org/xwalk/core/internal/XWalkContent.java
@@ -688,9 +688,10 @@ class XWalkContent extends FrameLayout implements XWalkPreferencesInternal.KeyVa
     @Override
     public void onKeyValueChanged(String key, XWalkPreferencesInternal.PreferenceValue value) {
         if (key == null) return;
+        enableRemoteDebugging();
         if (key.equals(XWalkPreferencesInternal.REMOTE_DEBUGGING)) {
-            if (value.getBooleanValue()) enableRemoteDebugging();
-            else disableRemoteDebugging();
+            //if (value.getBooleanValue()) enableRemoteDebugging();
+            //else disableRemoteDebugging();
         } else if (key.equals(XWalkPreferencesInternal.ENABLE_JAVASCRIPT)) {
             if (mSettings != null) {
                 mSettings.setJavaScriptEnabled(value.getBooleanValue());
